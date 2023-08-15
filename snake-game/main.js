fc36780f8e1f5e064684016e375d43763b735c10
Martin Rusev <marto260503@abv.bg>
2023-08-15 11:25:41 +0300

    Create class Player and setup the local storage

--- snake-game/main.js
+++ snake-game/main.js
@@ -3,30 +3,6 @@ import { GameBoard } from './Classes/GameBoard';
 import { direction } from './utils/utils';
 import { Player } from './Classes/Player';
 
-// const scoreList = document.getElementById('score-list');
-
-// function storage() {
-//   const readyStorage = JSON.parse(localStorage.getItem('storage'));
-//   const sortedData = readyStorage.sort((a, b) => {
-//     if (b.score === a.score) {
-//       return b.date - a.date
-//     }
-//     return b.score - a.score
-//   })
-
-//   scoreList.innerHTML = ''
-//   for (const item of sortedData) {
-//     const player = item.player;
-//     const score = item.score;
-//     const date = new Date(item.date).toString();
-
-//     const formattedText = `Player: ${player}, Score: ${score}, Date: ${date}`;
-//     const li = document.createElement('li');
-//     li.textContent = formattedText;
-//     scoreList.appendChild(li);
-
-//   }
-// }
 let gameBoard = new GameBoard(20, 20);
 let div = document.getElementById('table');
 gameBoard.table.initTableNode();
